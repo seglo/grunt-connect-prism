f5448fcdda8e0af3dc5f9b7cf7e5e3c0ec8a3604
seglo <seglo@randonom.com>
2014-09-30 00:09:58 -0400

    Use grunt instance passed into plugin.

--- tasks/prism-task.js
+++ tasks/prism-task.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.registerTask('prism', 'Configure any specified connect proxies for prism.', require('../lib/prism'));
+  grunt.registerTask('prism', 'Configure any specified connect proxies for prism.', function(targetArg, modeOverride) {
+    return require('../lib/prism')(targetArg, modeOverride, grunt);
+  });
 };
